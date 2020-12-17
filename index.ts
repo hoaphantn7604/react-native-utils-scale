765bcb7b958f18dfdd57fd548ff6c52302094771
Hoa Phan <hoa.phan-van@banvien.com.vn>
2020-12-17 11:25:27 +0700

    fix bug

--- index.ts
+++ index.ts
@@ -35,11 +35,7 @@ export const responsiveHeight = (height: number) => {
 };
 
 export function isIphoneX() {
-  const dimen = Dimensions.get('window');
-  return (
-    Platform.OS === 'ios' &&
-    (dimen.height === 812 || dimen.width === 812 || dimen.height === 896 || dimen.width === 896)
-  );
+  return DeviceInfo.hasNotch();
 }
 
 export const dimensionsScale = {
